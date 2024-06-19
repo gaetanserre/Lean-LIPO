2e9563c5ad18cc033cded2801d930639d724f1b9
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2024-06-19 12:40:18 +0200

    diam

--- LeanLIPO/Basic.lean
+++ LeanLIPO/Basic.lean
@@ -4,10 +4,12 @@ open Metric Pointwise Finset MeasureTheory ENNReal
 
 variable {d : ℕ} (hd : 0 < d) [Nonempty (Fin d)]
 
-lemma image_nonempty {f : EuclideanSpace ℝ (Fin d) → ℝ} {A : Finset (EuclideanSpace ℝ (Fin d))} (ha : A.Nonempty) : (A.image f).Nonempty :=
+lemma image_nonempty {f : EuclideanSpace ℝ (Fin d) → ℝ} {A : Finset (EuclideanSpace ℝ (Fin d))}
+    (ha : A.Nonempty) : (A.image f).Nonempty :=
   (Finset.image_nonempty).mpr ha
 
-theorem reject_iff_ball (f : EuclideanSpace ℝ (Fin d) → ℝ) {A : Finset (EuclideanSpace ℝ (Fin d))} (hA : A.Nonempty) {κ : ℝ} (hκ : 0 < κ) (x : EuclideanSpace ℝ (Fin d)) :
+theorem reject_iff_ball (f : EuclideanSpace ℝ (Fin d) → ℝ) {A : Finset (EuclideanSpace ℝ (Fin d))}
+    (hA : A.Nonempty) {κ : ℝ} (hκ : 0 < κ) (x : EuclideanSpace ℝ (Fin d)) :
     (A.image (fun y ↦ f y + κ * ‖x - y‖)).min' (image_nonempty hA)
       < (A.image f).max' (image_nonempty hA)
     ↔ ∃ x₁ ∈ A, x ∈ ball x₁ (((A.image f).max' (image_nonempty hA) - f x₁) / κ) := by
@@ -38,7 +40,8 @@ theorem reject_iff_ball_set (f : EuclideanSpace ℝ (Fin d) → ℝ) (A : Finset
   ext x
   exact reject_iff_ball f hA hκ x
 
-example (x₁ x₂ : EuclideanSpace ℝ (Fin d)) (r₁ r₂ : ℝ) (h : r₁ ≤ r₂) : volume (ball x₁ r₁) ≤ volume (ball x₂ r₂) := by
+lemma volume_mono (x₁ x₂ : EuclideanSpace ℝ (Fin d))
+    (r₁ r₂ : ℝ) (h : r₁ ≤ r₂) : volume (ball x₁ r₁) ≤ volume (ball x₂ r₂) := by
   repeat rw [EuclideanSpace.volume_ball, Fintype.card_fin]
   have h1 : ENNReal.ofReal (√Real.pi ^ d / ((d : ℝ) / 2 + 1).Gamma) ≠ 0 := by
     suffices 0 < √Real.pi ^ d / ((d : ℝ) / 2 + 1).Gamma by
@@ -50,5 +53,21 @@ example (x₁ x₂ : EuclideanSpace ℝ (Fin d)) (r₁ r₂ : ℝ) (h : r₁ ≤
     exact div_pos sqrt_pi_pos (Real.Gamma_pos_of_pos arg_pos)
   rw [mul_le_mul_right h1 (ofReal_ne_top)]
   exact pow_le_pow_left' (ofReal_le_ofReal h) d
+
+def argmax {α β : Type*} [LE β] (f : α → β) := {y | ∀ x, f x ≤ f y }
+def argmin {α β : Type*} [LE β] (f : α → β) := {y | ∀ x, f y ≤ f x }
+
+example (f : EuclideanSpace ℝ (Fin d) → ℝ)
+    {A : Finset (EuclideanSpace ℝ (Fin d))} (hA : A.Nonempty) :
+    ∀ x ∈ A, ∀ x' ∈ argmax f, ∀ x'' ∈ argmin f,
+    (A.image f).max' (image_nonempty hA) - f x
+    ≤ f x' - f x'' := by
+  intro x _ x' hx' x'' hx''
+  have image_le_max : (A.image f).max' (image_nonempty hA) ≤ f x' := by
+    have : ∃ x₁ ∈ A, f x₁ = (A.image f).max' (image_nonempty hA) := mem_image.mp (max'_mem _ (image_nonempty hA))
+    rcases this with ⟨x₁, _, hfx₁⟩
+    rw [← hfx₁]
+    exact hx' x₁
+  exact tsub_le_tsub image_le_max (hx'' x)
 
 #minimize_imports
